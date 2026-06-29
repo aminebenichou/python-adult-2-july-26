18e29db29377db74b9c656f4aa541daa716c8e73
aminebenichou <aamine060@gmail.com>
2026-06-29 10:52:11 +0100

    sync

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
 
+
+class TaskCard(QtWidgets.QWidget):
+
+    def __init__(self, title: str, desc:str = ""):
+        super().__init__()
+        self.card = QtWidgets.QWidget()
+
+
+        self.title = QtWidgets.QLabel(title)
+        self.desc = QtWidgets.QLabel(desc)
+
+        self.edit_btn = QtWidgets.QPushButton("Edit")
+        self.delete_btn = QtWidgets.QPushButton("Delete")
+
+        self.layout = QtWidgets.QVBoxLayout(self.card)
+        self.layout.addWidget(self.title)
+    
+    def display_card(self):
+
+        return self.card
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -13,12 +34,21 @@ class MyWidget(QtWidgets.QWidget):
         self.title_input = QtWidgets.QLineEdit(placeholderText="Enter Title")
         self.desc_input = QtWidgets.QTextEdit(placeholderText="Enter a Description")
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.title_input)
-        self.layout.addWidget(self.desc_input)
-        self.layout.addWidget(self.button)
+        self.main_layout =QtWidgets.QHBoxLayout(self)
+
+        self.left_layout = QtWidgets.QVBoxLayout()
+        self.left_layout.addWidget(self.title_input)
+        self.left_layout.addWidget(self.desc_input)
+        self.left_layout.addWidget(self.button)
+
+        self.right_layout = QtWidgets.QVBoxLayout()
+        self.right_layout.addWidget(TaskCard(title="hello").display_card())
 
         self.button.clicked.connect(self.magic)
+        
+        self.main_layout.addLayout(self.left_layout)
+        self.main_layout.addLayout(self.right_layout)
+
 
     @QtCore.Slot()
     def magic(self):
